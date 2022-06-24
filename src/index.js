febbc739e6f39ec7e0a3f72d1b4a89d55cf552f1
beyond_th0ught <yadavkhushboo7653@gmail.com>
2022-06-24 17:30:01 +0530

    first_project_heroku

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom';
+import App from './app.js'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(
+	<>
+		<App/>
+	</>,
+ 	document.getElementById('root')
+	);
+
+//(Q1) jsx use and what is babel and why we use babel
+
+//1st method
+// ReactDom.render(
+// 	react.createElement("h1",null,"Khushboo yadav")
+// 	, document.getElementById("root")
+// );
+
+//2nd method
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Tejas Thakare";
+// document.getElementById("root").appendChild(h1);
+
+//(Q2) how to render mumliple Elements inside ReactDom.render()?
+
+//1st method
+// ReactDom.render(
+// 	[
+// 	<h1>Khushboo yadav </h1>,
+// 	<p>Tejas Thakare</p>
+// 	], 
+// document.getElementById("root")
+// );
+
+//2nd method
+// ReactDom.render(
+// 	<div>
+// 	<h1>Khushboo yadav </h1>
+// 	<p>Tejas Thakare</p>
+// 	</div>, 
+// document.getElementById("root")
+// );
+
+//3rd method
+// ReactDom.render(
+// 	<>
+// 	<h1>Khushboo yadav </h1>
+// 	<p>Tejas Thakare</p>
+// 	</>, 
+// document.getElementById("root")
+// );
+
+//(Q3) understanding  React Fragment 
+// ReactDom.render(
+// 	<React.Fragment>
+// 	<h1>heyy its me Khushboo</h1>
+// 	<p>he Khushboo how are you</p>
+// 	<>
+// 		<img src={ Cat } className="IMG"/>
+// 	</>
+// 	<>
+// 		<img src={ Cat } className="IMG"/>
+// 	</>
+// 	</React.Fragment>,
+// 	document.getElementById("root")
+// );
+// ReactDom.render(
+// 	<div>
+// 	<h1>heyy its me Khushboo</h1>
+// 	<p>he Khushboo how are you!!</p>
+// 	</div>,
+// 	document.getElementById("root")
+// );
